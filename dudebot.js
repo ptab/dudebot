954ed7c7efcd375058db941e11d8e6bc085ff2b3
ptaborda <pedro@taborda.me>
2016-04-24 17:04:46 +0200

    Separated ask intents

--- dudebot.js
+++ dudebot.js
@@ -15,87 +15,92 @@ var https = require('https');
 var tabletojson = require('tabletojson');
 
 var controller = require('botkit').slackbot({ debug: false });
-var slackbot = controller.spawn({ token: getToken('slack') }).startRTM();
+var slackbot = controller.spawn({ token: getToken('slack') }).startRTM((err, bot, payload) =>  {
+    if (err) throw new Error('Error connecting to Slack: ', err)
+});
 var witbot = require('witbot')(getToken('wit'));
 
 controller.on(['direct_message', 'direct_mention', 'mention', 'ambient'], (bot, message) => {
+    var confidenceToAsk = initialConfidence(message.event) ;
+    var confidenceToReply = config.confidence.reply ;
+
+    var tool;
+    var environment;
+
+    var askForEnvironment = function(convo) {
+        convo.ask(msg(message.user, 'on which environment?'), (response, convo) => {
+            witbot
+            .process(response.text)
+            .hears('reply_with_environment', config.confidence.reply, (outcome) => {
+                environment = outcome.entities.environment[0].value;
+                convo.next();
+            })
+            .otherwise((outcome) => {
+                log(outcome);
+                bot.reply(message, 'uh? I don\'t know that one');
+                convo.repeat();
+                convo.next();
+            });
+        });
+    };
+
+
     witbot
     .process(message.text)
-    .hears('ask_for_tool', initialConfidence(message.event), (outcome) => {
-        log(outcome);
-        var tools = outcome.entities.tool;
-        var environments = outcome.entities.environment;
+    .hears('ask_with_both', confidenceToAsk, (outcome) => {
+        tool = outcome.entities.tool[0].value;
+        environment = outcome.entities.environment[0].value;
+        reply(message, tool, environment);
+    })
+    .hears('ask_with_tool', confidenceToAsk, (outcome) => {
+        tool = outcome.entities.tool[0].value;
 
-        var tool;
-        var environment;
-
-        var askForTool = function(convo, ask_for_environment) {
+        slackbot.startConversation(message, (_, convo) => {
+            askForEnvironment(convo);
+            convo.on('end', (convo) => {
+                if (convo.status == 'completed') {
+                    reply(message, tool, environment);
+                }
+            });
+        });
+    })
+    .hears('ask_with_none', confidenceToAsk, (outcome) => {
+        slackbot.startConversation(message, (_, convo) => {
             convo.ask(msg(message.user, 'which tool?'), (response, convo) => {
                 witbot
                 .process(response.text)
-                .hears('reply_tool', config.confidence.reply, (outcome) => {
-                    log(outcome);
-                    tool = outcome.entities.tool[0].value ;
-                    if (empty(environments)) askForEnvironment(convo);
-                    convo.next();
-                })
-                .hears('reply_both', config.confidence.reply, (outcome) => {
+                .hears('reply_with_both', confidenceToReply, (outcome) => {
                     log(outcome);
                     tool = outcome.entities.tool[0].value ;
                     environment = outcome.entities.environment[0].value;
                     convo.next();
                 })
-                .otherwise((outcome) => {
-                    logTooLow(outcome);
-                    bot.reply(message, 'uh? I don\'t know that one');
-                    convo.repeat();
-                    convo.next();
-                });
-            });
-        } ;
-
-        var askForEnvironment = function(convo) {
-            convo.ask(msg(message.user, 'on which environment?'), (response, convo) => {
-                witbot
-                .process(response.text)
-                .hears('reply_environment', config.confidence.reply, (outcome) => {
-                    environment = outcome.entities.environment[0].value;
+                .hears('reply_with_tool', confidenceToReply, (outcome) => {
+                    log(outcome);
+                    tool = outcome.entities.tool[0].value ;
+                    askForEnvironment(convo);
                     convo.next();
                 })
                 .otherwise((outcome) => {
-                    logTooLow(outcome);
+                    log(outcome);
                     bot.reply(message, 'uh? I don\'t know that one');
                     convo.repeat();
                     convo.next();
                 });
             });
-        } ;
 
-        var endConversation = function(convo) {
-            if (convo.status == 'completed') {
-                if (empty(tool)) tool = tools[0].value;
-                if (empty(environment)) environment = environments[0].value;
-                reply(message, tool, environment);
-            }
-        };
-
-        if (empty(tools)) {
-            slackbot.startConversation(message, (_, convo) => {
-                askForTool(convo, empty(environments));
-                convo.on('end', endConversation);
+            convo.on('end', (convo) => {
+                if (convo.status == 'completed') {
+                    reply(message, tool, environment);
+                }
             });
-        } else if (empty(environments)) {
-            slackbot.startConversation(message, (_, convo) => {
-                askForEnvironment(convo);
-                convo.on('end', endConversation);
-            });
-        } else {
-            var tool = tools[0].value;
-            var environment = environments[0].value;
-            reply(message, tool, environment) ;
-        }
+        });
+    })
+    .otherwise((outcome) => {
+        logTooLow(outcome);
     });
 });
+
 
 
 function reply(message, tool, environment) {
@@ -141,24 +146,12 @@ function initialConfidence(event) {
     else return config.confidence.reply;
 }
 
-function empty(obj) {
-    return !obj || obj.length === 0;
-}
-
 function getToken(name) {
     var token = config.tokens[name] ;
-    if (token) {
-        return token;
-    } else {
-        console.error('Error: token for', name, 'is not defined');
-        process.exit(1);
-    }
+    if (token) return token;
+    else throw new Error('Token for ' +  name + ' is not defined');
 }
 
 function log(outcome) {
     console.log('Heard', outcome.intent, 'with', outcome.confidence, 'confidence for:', outcome._text);
-}
-
-function logTooLow(outcome) {
-    console.log('Confidence', outcome.confidence, 'too low for:', outcome._text);
 }
